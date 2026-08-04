3ca52e896a473d804852db7f2cf86e193c33a8ca
Megha Shivaji Kammar <megahkammar@gmail.com>
2026-08-04 11:58:08 +0530

    Added Weekdays.java

---
+++ track/M02-INR-TO-JAVA/T01/Weekdays.java.java
@@ -0,0 +1,13 @@
+
+public class Weekdays {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
