887845e2af34e3f7b18b1e3f43b559f01b130fc4
Megha Shivaji Kammar <megahkammar@gmail.com>
2026-08-15 19:24:58 +0530

    Added LearnerDetails program

---
+++ track/M02-INR-TO-JAVA/T01/LearnerDetails.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class LearnerDetails {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+        int solvedProblems = scanner.nextInt();
+        double assessmentPercentage = scanner.nextDouble();
+
+        System.out.println("Learner: " + firstName);
+        System.out.println("Problems solved: " + solvedProblems);
+        System.out.println("Assessment: " + assessmentPercentage);
+
+        scanner.close();
+    }
+}
