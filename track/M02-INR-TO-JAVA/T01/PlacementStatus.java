1d9b3ab256f6a3803810d692793d0b0bd1d0c3a9
Megha Shivaji Kammar <megahkammar@gmail.com>
2026-08-15 19:18:38 +0530

    Added PlacementStatus program

---
+++ track/M02-INR-TO-JAVA/T01/PlacementStatus.java
@@ -0,0 +1,16 @@
+
+public class PlacementStatus {
+
+    public static void main(String[] args) {
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+
+        String status = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continues Preparation";
+        System.out.println(status);
+
+        for (int i = 1; i <= practiceDays; i++) {
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
