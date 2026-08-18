95b56ce6f52a7a76e53eeb036c7f5ebad6bfb54c
Megha Shivaji Kammar <megahkammar@gmail.com>
2026-08-18 11:40:42 +0530

    Add Array1 program

---
+++ track/Array/Array1.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a[] = new int[5];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println("Array Element are:");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.print(a[i] + " ");
+        }
+        int sum = 0;
+        for (int i = 0; i <= a.length - 1; i++) {
+            sum = sum + a[i];
+        }
+        System.out.println();
+        System.out.println("Total:" + sum);
+    }
+}
