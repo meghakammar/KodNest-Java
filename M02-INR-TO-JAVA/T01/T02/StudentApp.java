0daa19efc12b5924e61230a496a1c3afd2caa92d
Megha Shivaji Kammar <megahkammar@gmail.com>
2026-08-25 19:58:13 +0530

    Add StudentApp

---
+++ M02-INR-TO-JAVA/T01/T02/StudentApp.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class Student {
+
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("Student Name: " + name);
+    }
+}
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String fullName = scanner.nextLine();
+        Student student = new Student(fullName);
+        student.display();
+        scanner.close();
+    }
+}
